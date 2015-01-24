fc1aafda5ca841da95b90976c3c56365e5d9012e
Andrew <chenpoanandrew@gmail.com>
2015-01-24 14:30:59 +0800

    多層樹狀完成 第二層之後定位有點小問題 可以更好

--- Andrew/js/hw1_linkbar.js
+++ Andrew/js/hw1_linkbar.js
@@ -1,32 +1,17 @@
 $(document).ready(function () {
 
-	//root variable
-	var linkbarInfo = $('#linkbarInfo>li');
+	$(".list").slideUp(0);
 
-	var rootTable = '<div class="root">@text</div>';
-	var childTable = '<div class="child">@text</div>';
-	var rootWrapper = '<div class="rootWrapper"></div>';
-
-	//add root text
-	for (var i = 0; i < linkbarInfo.length; i++) {
-		$('div#linkbar').append(rootTable.replace('@text', $(linkbarInfo[i]).children("p").text()));
-
-		if ($(linkbarInfo[i]).children("ul").length != 0) {
-
-			var childInfo = $(linkbarInfo[i]).find("ul>li>p");
-			for (var j = 0; j < childInfo.length; j++) {
-				$('div#linkbar>div.root:last-child').append(childTable.replace('@text', $(childInfo[j]).text()));
-			}
-		}
-	}
+	$("div.btn").hover(function () {
+		$(this).children(".list").slideDown('fast');
+	}, function () {
+		$(this).children(".list").slideUp('fast');
+	});
 	
-	$('.child').slideUp(0);
-});
-
-$('html, body').on('mouseover', '.root', function () {
-	$(this).children('.child').slideDown('fast');
-});
-
-$('html, body').on('mouseleave', '.root', function () {
-	$(this).children('.child').slideUp('fast');
+	$(".list>div").hover(function () {
+		$(this).children(".list").slideDown('fast');
+	}, function () {
+		$(this).children(".list").slideUp('fast');
+	});
+	
 });
