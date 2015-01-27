00bb1d013e3c80d43fce1c370ba79a0e5e2463fe
Andrew <chenpoanandrew@gmail.com>
2015-01-27 11:08:50 +0800

    ch13 泡沫事件

---
+++ Hsuan/connect.php
@@ -0,0 +1,6 @@
+<?php 
+	
+	 $link = mysqli_connect("localhost","root","root","oit_v3")or die("無法連接");
+     mysqli_query($link,'set names utf8');
+	 return $link;
+?>
--- Ray/HomeWork_1/sql_conn.php
+++ Ray/HomeWork_1/sql_conn.php
@@ -1,5 +1,5 @@
  <?php
-        $link = mysqli_connect("localhost","root","","oit_v2")or die("無法連接");
+        $link = mysqli_connect("localhost","root","root","oit_v3")or die("無法連接");
         mysqli_query($link,'set names utf8');
         return $link;
 ?>
