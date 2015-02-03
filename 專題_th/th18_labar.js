ac8bba2aea277970d19dc0af079b66b5522d7452
Andrew <chenpoanandrew@gmail.com>
2015-02-03 16:47:15 +0800

    修正了拉壩的判定,使用count 事先判定好後,在開始運轉

--- 專題_th/th18_labar.js
+++ 專題_th/th18_labar.js
@@ -3,21 +3,24 @@ $(document).ready(function () {
 	startAt(1);
 	startAt(2);
 	startAt(3);
-	
+
 	function startAt(num) {
 		var ctrl = true;
 		var box = $('#cal' + num.toString());
 
 		var index = 1;
-		var img = '<img id="in" src="../Doro/SlotMachine/rila_@num.png" alt="">';
+		var img = '<img src="../Doro/0122/ImageCarousel/rila_@num.jpg" alt="">';
+
+		var count = 0;
+		var limit = Math.floor((Math.random() * 10) + 10);
 
 		setInterval(function () {
-			if ((Math.floor((Math.random() * 10) + 1) < 9) && ctrl == true) {
+			if (count++ <= limit && ctrl == true) {
 				$(box).append(img.replace('@num', index++)); //insert a pic
-				if (index == 6) {
+				if (index == 4) {
 					index = 1;
 				}
-				show($('#cal'+num.toString()+'>img:last-child'));
+				show($('#cal' + num.toString() + '>img:last-child'));
 			} else {
 				ctrl = false;
 			}
@@ -37,7 +40,9 @@ $(document).ready(function () {
 				$(obj).animate({
 					top: "+=200px",
 					opacity: 0
-				}, 300);
+				}, 300, function(){
+					$(this).remove();
+				});
 			});
 		}
 	}
